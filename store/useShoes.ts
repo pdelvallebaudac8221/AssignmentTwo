63645fd03b05dd1a5a5ddc24d5e8447a6e7f840f
Piero del Valle <pdelvallebaudac8221@conestogac.on.ca>
2025-11-19 15:56:12 -0500

    add visited count

--- store/useShoes.ts
+++ store/useShoes.ts
@@ -6,15 +6,17 @@ interface ShoeState {
   shoes: Shoes[],
   favoriteShoes: number[],
   visitedShoes: number[],
+  visitedCount: () => number,
   addFavorite: (id: number) => void,
   removeFavorite: (id: number) => void,
   setShoeAsVisited: (id: number) => void,
 }
 
-const useShoes = create((set) => ({
+const useShoes = create<ShoeState>((set, get) => ({
   shoes: SHOES_ARRAY,
   favoriteShoes: [],
   visitedShoes: [],
+  visitedCount: () => get().visitedShoes.length,
   addFavorite: (id: number) => set((state: ShoeState) => ({
     favoriteShoes: [...state.favoriteShoes, id]
   })),
