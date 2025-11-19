274703097cbc59f27669bac3f922d20a5380e3c3
Piero del Valle <pdelvallebaudac8221@conestogac.on.ca>
2025-11-19 15:52:26 -0500

    add visited methods

--- store/useShoes.ts
+++ store/useShoes.ts
@@ -5,17 +5,27 @@ import { Shoes } from "../types/Shoes";
 interface ShoeState {
   shoes: Shoes[],
   favoriteShoes: number[],
-  addFavorite: (id: number) => void
-  removeFavorite: (id: number) => void
+  visitedShoes: number[],
+  addFavorite: (id: number) => void,
+  removeFavorite: (id: number) => void,
+  setShoeAsVisited: (id: number) => void,
 }
 
 const useShoes = create((set) => ({
   shoes: SHOES_ARRAY,
   favoriteShoes: [],
+  visitedShoes: [],
   addFavorite: (id: number) => set((state: ShoeState) => ({
     favoriteShoes: [...state.favoriteShoes, id]
   })),
   removeFavorite: (id: number) => set((state: ShoeState) => ({
     favoriteShoes: state.favoriteShoes.filter((s: number) => s !== id)
   })),
+  setShoeAsVisited: (id: number) => set((state: ShoeState) => {
+    if (state.visitedShoes.includes(id)) {
+      return state
+    } else {
+      return { visitedShoes: [...state.visitedShoes, id]}
+    }
+  })
 }))
